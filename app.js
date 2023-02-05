f01a68b712bfe4d8ddb2de7dae059b721b07f754
Sova <boquiren40@gmail.com>
2023-02-06 02:55:38 +0800

    third commit

--- app.js
+++ app.js
@@ -146,6 +146,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen('/.netlify/functions/api', function() {
   console.log("Server started on port 3000");
 });
+module.exports.handler = serverless(app);
